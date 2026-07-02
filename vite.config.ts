6737540c3a924d609f0891d54b03fa2edd64d95b
Michael Romashov <mromashov@icloud.com>
2026-07-02 18:18:06 -0400

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "ecl",
     plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
